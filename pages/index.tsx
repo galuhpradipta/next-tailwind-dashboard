f48509e08541eae6bcb043cc9e8bedb3b26a9a40
Galuh Pradipta <galuh.pradipta@brank.as>
2022-06-24 12:44:41 +0700

    add ts and tailwind

---
+++ pages/index.tsx
@@ -0,0 +1,14 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <p className='text-center'>Hello World</p>
+    </div>
+  )
+}
+
+export default Home
